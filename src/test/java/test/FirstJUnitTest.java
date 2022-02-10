ad542af2b7cd30b38a1a6c56fdfe5940db272ef4
lmaslo <PerepechkinaEV@yandex.ru>
2022-02-10 14:03:12 +0300

    new files

---
+++ src/test/java/test/FirstJUnitTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class FirstJUnitTest {
+    @Test
+    void successFillTest() {
+        open("https://demoqa.com/automation-practice-form");
+    }
+}
+
