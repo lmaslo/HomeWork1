fa63b4c942dbf6fc585b204b65305d356f82d57a
lmaslo <PerepechkinaEV@yandex.ru>
2022-02-10 15:30:21 +0300

    new files and setting

--- src/test/java/test/FirstJUnitTest.java
+++ src/test/java/test/FirstJUnitTest.java
@@ -1,13 +1,41 @@
 package test;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class FirstJUnitTest {
+   @BeforeAll
+   static void beforeAll() {
+       open("https://demoqa.com/automation-practice-form");
+   }
+
     @Test
     void successFillTest() {
-        open("https://demoqa.com/automation-practice-form");
+        $("#firstName").setValue("Lena");
+        $("#lastName").setValue("Masloboishchikova");
+        $("#userEmail").setValue("lMaslo@mail.ru");
+        //gender
+        //$("enterWrapper").selectRadio("Female");
+
+
+
+        $("#userNumber").setValue("8945632145");
+        //date
+        //Subjects
+        //hobbies
+        //Picture
+        $("#currentAddress").setValue("Address 1 2 3 4 5");
+        //state
+        //city
+
+
+
+
     }
 }
 
