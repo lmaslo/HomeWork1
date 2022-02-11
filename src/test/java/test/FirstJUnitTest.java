5a9ec96d2f3eb81ec41d983f6fa0e43f0d233ed1
lmaslo <PerepechkinaEV@yandex.ru>
2022-02-11 07:38:50 +0300

    new files and setting

--- src/test/java/test/FirstJUnitTest.java
+++ src/test/java/test/FirstJUnitTest.java
@@ -2,6 +2,7 @@ package test;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.Configuration;
 
 
 import static com.codeborne.selenide.Condition.text;
@@ -12,11 +13,15 @@ import static com.codeborne.selenide.Selenide.open;
 public class FirstJUnitTest {
     @BeforeAll
     static void beforeAll() {
-        open("https://demoqa.com/automation-practice-form");
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+
     }
 
     @Test
     void successFillTest() {
+
+        open("/automation-practice-form");
         $(".main-header").shouldHave(text("Practice Form"));
 
         $("#firstName").setValue("Lena");
@@ -25,14 +30,20 @@ public class FirstJUnitTest {
         //gender, как работает selectRadio("Female")
         $(byText("Female")).click();
         $("#userNumber").setValue("8945632145");
-
-
-        //date
-        //Subjects
-        //hobbies
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").selectOptionByValue("1993");
+        $(".react-datepicker__month-select").selectOptionByValue("8");
+        $(byText("10")).click();
+        $("#subjectsInput").setValue("English").pressEnter();
+        $(byText("Sports")).click();
         //Picture
+
+
         $("#currentAddress").setValue("Address 1 2 3 4 5");
         //state
+       // $(".css-1hwfws3").click();
+       // $(".css-yk16xz-control").selectOptionByValue("NCR");
+
         //city
 
 
